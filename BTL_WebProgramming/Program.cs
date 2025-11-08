659360e7c823710873c2340d15fc3be423ffd71d
khanhko09 <khanhko09100@gmail.com>
2025-11-08 14:16:26 +0700

    change

--- BTL_WebProgramming/Program.cs
+++ BTL_WebProgramming/Program.cs
@@ -33,7 +33,7 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Main}/{id?}")
+    pattern: "{controller=Auth}/{action=DangNhap}/{id?}")
     .WithStaticAssets();
 
 app.UseSession();
